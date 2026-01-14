5b21be733e63ffe388837df95c439a5cf2eb8ec7
Kata <kata@dreamverse.ng>
2026-01-14 04:15:00 +0100

    build: update build flow

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
-export default {
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
   server: {
     open: true
   },
@@ -6,6 +9,13 @@ export default {
     open: true
   },
   build: {
-    outDir: "dist"
-  }
-}
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        dreamstack: resolve(__dirname, 'dreamstack/index.html'),
+        review: resolve(__dirname, 'dreamstack/copilot-review.html'),
+      },
+    },
+  },
+})
